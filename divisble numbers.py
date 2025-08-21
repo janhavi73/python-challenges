f02bfc43cadce210d42ca50aa6a6699e12af6d3e
janhavi73 <janhavisinghania0607@gmail.com>
2025-08-21 11:30:57 +0100

    Add files via upload

---
+++ divisble numbers.py
@@ -0,0 +1,11 @@
+print("please enter a (numerator)")
+numn=int(input())
+print("please enter a (denominator)")
+numd=int(input())
+
+if numn%numd==0:
+    print(numd,"is divisble by",numn)
+else:
+    print(numd,"is not divisble by",numn)
+
+
